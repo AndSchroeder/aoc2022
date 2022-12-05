400181dd7033ca9bf8d3c65a83e8c97b035482d2
Andi Schröder <andreas.schroeder@iits-consulting.de>
2022-12-05 08:43:58 +0100

    feat: solve day 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import day.*
 
 fun main(args: Array<String>) {
 
-  val days = listOf(Day01, Day02, Day03, Day04)
+  val days = listOf(Day01, Day02, Day03, Day04, Day05)
   days.forEach(::solveDay)
 }
 
---
+++ src/main/kotlin/day/Day05.kt
@@ -0,0 +1,81 @@
+package day
+
+import util.FileReader
+
+
+object Day05 : Day {
+    override val examplePartOneSolution: String = "CMZ"
+    override val examplePartTwoSolution: String = "MCD"
+
+    override fun examplePartOne() = FileReader.getExampleList(5).organizeOne()
+        .apply { check(this == examplePartOneSolution) }
+
+    override fun examplePartTwo() = FileReader.getExampleList(5).organizeTwo()
+        .apply { check(this == examplePartTwoSolution) }
+
+    override fun solvePartOne() = FileReader.getInputList(5).organizeOne()
+    override fun solvePartTwo() = FileReader.getInputList(5).organizeTwo()
+
+
+    private fun List<String>.organizeOne(): String {
+        val (stack, operations) = this.organize()
+        operations.forEach { handleOperationOne(stack, it) }
+        return stack.joinToString("") { it.first() }
+    }
+
+    private fun List<String>.organizeTwo(): String {
+        val (stack, operations) = this.organize()
+        operations.forEach { handleOperationTwo(stack, it) }
+        return stack.joinToString("") { it.first() }
+    }
+
+    private fun List<String>.organize() =
+        this.getStackData().toInputList().toStack() to this.getOperationsData().getOperations()
+
+
+    private fun handleOperationOne(stacks: List<ArrayDeque<String>>, operations: Triple<Int, Int, Int>) {
+        val (times, from, to ) = operations
+        repeat(times) {
+            stacks[to].addFirst(stacks[from].removeFirst())
+        }
+    }
+    private fun handleOperationTwo(stacks: List<ArrayDeque<String>>, operations: Triple<Int, Int, Int>) {
+        val (times, from, to ) = operations
+        stacks[from].take(times).reversed().forEach {
+            stacks[to].addFirst(it)
+        }
+        repeat(times) {
+            stacks[from].removeFirst()
+        }
+    }
+
+    private fun List<String>.getStackData() = this.filter{ it.contains("[")}
+
+    private fun List<String>.getOperationsData() = this.filter{ it.contains("move")}
+
+    private fun List<String>.toInputList() = this.getStackData().map { row -> row.chunked(4).map { entry -> entry.replace("""\W""".toRegex(),"") } }
+
+    private fun List<List<String>>.toStack(): MutableList<ArrayDeque<String>> {
+        val entries = this.maxOf { it.size }
+        val stacks = MutableList(entries) {ArrayDeque<String>()}
+        this.forEach {row ->
+            row.forEachIndexed { index, entry ->
+                if (entry.isNotBlank()) {
+                    stacks[index].addLast(entry)
+                }
+            }
+        }
+        return stacks
+    }
+
+    private fun List<String>.getOperations() = this.map {it.toOperationInput()}
+
+    private fun String.toOperationInput() = this.split("""\D""".toRegex()).filter(String::isNotBlank).run {
+        Triple(get(0).toInt(), get(1).toInt() - 1, get(2).toInt() -1)
+    }
+
+
+
+
+
+}
