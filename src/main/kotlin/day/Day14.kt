d0a86d1439a3ee4cef54739daaf03f2903203500
Andi Schröder <andreas.schroeder@iits-consulting.de>
2022-12-14 09:18:01 +0100

    feat: solve day 14

---
+++ src/main/kotlin/day/Day14.kt
@@ -0,0 +1,88 @@
+package day
+
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+object Day14 : Day("14", "24", "93") {
+    override fun examplePartOne() = getExampleList().solve()
+    override fun examplePartTwo() = getExampleList().solve(false)
+    override fun solvePartOne() = getInputList().solve()
+    override fun solvePartTwo() = getInputList().solve(false)
+
+    fun List<String>.solve(withAbyss: Boolean = true): String {
+        val grid = parse(withAbyss)
+        return grid.fields.count { it.content == CaveContent.SAND }.toString()
+    }
+
+    private fun List<String>.parse(withAbyss: Boolean) = CaveGrid().also { grid ->
+        parseRocks(grid, withAbyss)
+        while (!grid.ended) {
+            grid.trickle(withAbyss = withAbyss)
+        }
+    }
+
+    private fun List<String>.parseRocks(grid: CaveGrid, withAbyss: Boolean) = forEach { row ->
+        row.split(" -> ").map { it.split(",").map(String::toInt) }.zipWithNext() { first, second ->
+            grid.addRockLine(second.first(), second.last(), first.first(), first.last())
+        }
+    }.apply {
+        if (!withAbyss) {
+            val (xMin, xMax) = grid.fields.minBy { it.x }.x to grid.fields.maxBy { it.x }.x
+            grid.addRockLine(xMin - grid.lowest * 2, grid.lowest + 2, xMax + grid.lowest * 2, grid.lowest + 2)
+        }
+    }
+
+    data class CaveGrid(
+        val fields: MutableSet<CaveField> = mutableSetOf(),
+        var lowest: Int = 0,
+        var ended: Boolean = false
+    ) {
+
+        init {
+            fields.add(CaveField(500, 0, CaveContent.START))
+        }
+
+        fun addRockLine(xStart: Int, yStart: Int, xEnd: Int, yEnd: Int) = when (xStart) {
+            xEnd -> (min(yStart, yEnd) until max(yStart, yEnd) + 1).forEach { y -> addRock(xStart, y) }
+            else -> (min(xStart, xEnd) until max(xStart, xEnd) + 1).forEach { x -> addRock(x, yStart) }
+        }
+
+        fun trickle(x: Int = start()?.x ?: 500, y: Int = start()?.y ?: 0, withAbyss: Boolean = true) {
+            if (y > lowest && withAbyss || start() == null) {
+                ended = true
+                return
+            }
+            val firstNeighbor = neighbors(x, y).firstOrNull()
+            if (firstNeighbor == null) {
+                setSand(x, y)
+                return
+            }
+            trickle(firstNeighbor.x, firstNeighbor.y, withAbyss)
+        }
+
+        private fun setSand(x: Int, y: Int) {
+            findOrCreate(x, y).content = CaveContent.SAND
+        }
+
+        private fun neighbors(x: Int, y: Int) = listOf(
+            findOrCreate(x, y + 1), findOrCreate(x - 1, y + 1), findOrCreate(x + 1, y + 1),
+        ).filter { it.content == CaveContent.AIR || it.content == CaveContent.START }
+
+        private fun findOrCreate(x: Int, y: Int) =
+            fields.find { it.x == x && it.y == y } ?: CaveField(x, y, CaveContent.AIR).apply { fields.add(this) }
+
+        private fun start() = fields.firstOrNull() { it.content == CaveContent.START }
+
+        private fun addRock(x: Int, y: Int) =
+            fields.add(CaveField(x, y, CaveContent.ROCK)).apply { lowest = max(lowest, y) }
+    }
+
+    data class CaveField(val x: Int, val y: Int, var content: CaveContent)
+
+    enum class CaveContent {
+        AIR,
+        ROCK,
+        SAND,
+        START,
+    }
+}
